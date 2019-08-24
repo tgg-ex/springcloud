b670cbcd6662997186acfc2d62f424de4d098752
zyz <m13143401724@163.com>
2019-08-24 15:50:20 +0800

    添加配置中心依赖

--- demo-config/src/main/java/com/springcloud/demo/ConfigApplicaiton.java
+++ demo-config/src/main/java/com/springcloud/demo/ConfigApplicaiton.java
@@ -2,6 +2,7 @@ package com.springcloud.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -10,6 +11,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  * 配置中心启动类
  */
 @SpringBootApplication
+@EnableConfigServer
 @EnableEurekaClient
 public class ConfigApplicaiton {
     public static void main(String[] args) {
